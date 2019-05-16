b6da85adecb0f96855f2890393f948341f8bc8b7
Kavin <kavinapps@gmail.com>
2019-05-16 17:07:32 +0800

    update to support LG Capture+ and check path by contains.

--- app/src/main/java/com/abangfadli/shotwatchapp/MainActivity.java
+++ app/src/main/java/com/abangfadli/shotwatchapp/MainActivity.java
@@ -1,7 +1,11 @@
 package com.abangfadli.shotwatchapp;
 
+import android.Manifest;
+import android.content.pm.PackageManager;
 import android.net.Uri;
 import android.os.Bundle;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.widget.ImageView;
 import android.widget.TextView;
@@ -12,6 +16,7 @@ import com.abangfadli.shotwatch.ShotWatch;
 
 public class MainActivity extends AppCompatActivity {
 
+    private static final int MY_PERMISSIONS_REQUEST_READ_EXTERNAL_STORAGE = 200;
     private ShotWatch mShotWatch;
 
     private TextView mText;
@@ -24,6 +29,49 @@ public class MainActivity extends AppCompatActivity {
         mText = (TextView) findViewById(R.id.text);
         mImage = (ImageView) findViewById(R.id.image);
 
+
+        //Check Permission
+        if (ContextCompat.checkSelfPermission(this, Manifest.permission.READ_EXTERNAL_STORAGE)
+                != PackageManager.PERMISSION_GRANTED) {
+            // Permission is not granted
+            // Should we show an explanation?
+            if (ActivityCompat.shouldShowRequestPermissionRationale(this,
+                    Manifest.permission.READ_EXTERNAL_STORAGE)) {
+                // Show an explanation to the user *asynchronously* -- don't block
+                // this thread waiting for the user's response! After the user
+                // sees the explanation, try again to request the permission.
+            } else {
+                // No explanation needed; request the permission
+                ActivityCompat.requestPermissions(this,
+                        new String[]{Manifest.permission.READ_EXTERNAL_STORAGE},
+                        MY_PERMISSIONS_REQUEST_READ_EXTERNAL_STORAGE);
+                // MY_PERMISSIONS_REQUEST_READ_EXTERNAL_STORAGE is an
+                // app-defined int constant. The callback method gets the
+                // result of the request.
+            }
+        } else {
+            createShotWatch();
+        }
+
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        if(mShotWatch != null) {
+            mShotWatch.register();
+        }
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        if(mShotWatch != null) {
+            mShotWatch.unregister();
+        }
+    }
+
+    public void createShotWatch() {
         mShotWatch = new ShotWatch(getContentResolver(), new ShotWatch.Listener() {
             @Override
             public void onScreenShotTaken(ScreenshotData screenshotData) {
@@ -34,15 +82,29 @@ public class MainActivity extends AppCompatActivity {
         });
     }
 
-    @Override
-    protected void onResume() {
-        super.onResume();
-        mShotWatch.register();
-    }
 
     @Override
-    protected void onPause() {
-        super.onPause();
-        mShotWatch.unregister();
+    public void onRequestPermissionsResult(int requestCode,
+                                           String[] permissions, int[] grantResults) {
+        switch (requestCode) {
+            case MY_PERMISSIONS_REQUEST_READ_EXTERNAL_STORAGE: {
+                // If request is cancelled, the result arrays are empty.
+                if (grantResults.length > 0
+                        && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                    // permission was granted.
+                    createShotWatch();
+                    if(mShotWatch != null) {
+                        mShotWatch.register();
+                    }
+                } else {
+                    // permission denied, boo! Disable the
+                    // functionality that depends on this permission.
+                }
+                return;
+            }
+
+            // other 'case' lines to check for other
+            // permissions this app might request.
+        }
     }
 }
